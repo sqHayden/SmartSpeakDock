a65741e19ba99d3c3e52eed33433cc65d47a9ee5
Franck.ZY.Zhang <franck.zy.zhang@mail.foxconn.com>
2017-12-20 11:15:49 +0800

    standby chages v3
    Change-Id: I164561fa6c88d4f7895528fce88edcf714e1e2e0

---
+++ app/src/main/java/com/idx/smartspeakdock/standby/DataView.java
@@ -0,0 +1,19 @@
+package com.idx.smartspeakdock.standby;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextClock;
+
+/**
+ * Created by peter on 12/20/17.
+ */
+
+public class DataView extends TextClock {
+    public DataView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        setFormat12Hour("EEEE, MM月dd日");
+        setFormat24Hour("EEEE, MM月dd日");
+        setTypeface(FontCustom.setHeiTi(context));
+    }
+}
