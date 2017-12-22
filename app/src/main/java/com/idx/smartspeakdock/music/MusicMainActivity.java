ef82084d90dae7d4815ddbe3645337e4ea33d932
Ryan.FP.Chan <ryan.fp.chan@mail.foxconn.com>
2017-12-22 14:06:06 +0800

    weather get error 4.4 and Swipe fix and Music UI
    Change-Id: I6aeed4eb6854b0addeb1d3237bfda3125c7c219b

--- app/src/main/java/com/idx/smartspeakdock/music/MusicMainActivity.java
+++ app/src/main/java/com/idx/smartspeakdock/music/MusicMainActivity.java
@@ -1,9 +1,15 @@
 package com.idx.smartspeakdock.music;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.NavUtils;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.idx.smartspeakdock.R;
 
@@ -14,14 +20,63 @@ public  class MusicMainActivity extends AppCompatActivity  {
     private List<MusicBean> musicBeans = null;
     private MusicAdapter musicadapter;
     private ListView listview;
+    DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.music_activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle(getResources().getString(R.string.music_title));
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout.setStatusBarBackground(R.color.colorSelfBlack);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
+
         listview = (ListView) findViewById(R.id.music_list_view);
         musicBeans  = MusicUtil.getMp3Infos(MusicMainActivity.this);
         musicadapter = new MusicAdapter(this, musicBeans);
         listview.setAdapter(musicadapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.list_navigation_weather:
+                                NavUtils.navigateUpFromSameTask(MusicMainActivity.this);
+                                break;
+                            case R.id.list_navigation_music:
+                                // TODO: 17-12-16  Do nothing, we're already on that screen
+                                break;
+                            default:
+                                break;
+                        }
+
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
     }
 }
--- app/src/main/java/com/idx/smartspeakdock/standby/StandbyActivity.java
+++ app/src/main/java/com/idx/smartspeakdock/standby/StandbyActivity.java
@@ -1,7 +1,9 @@
 package com.idx.smartspeakdock.standby;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
@@ -9,6 +11,8 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextClock;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -22,11 +26,17 @@ import com.google.gson.Gson;
 import com.idx.smartspeakdock.BaseActivity;
 import com.idx.smartspeakdock.R;
 import com.idx.smartspeakdock.Swipe.SwipeActivity;
+import com.idx.smartspeakdock.start.StartActivity;
 import com.idx.smartspeakdock.weather.model.weather.Weather;
 import com.idx.smartspeakdock.weather.utils.HandlerWeatherUtil;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 
 public class StandbyActivity extends BaseActivity {
     private static final String TAG = "MainActivity";
@@ -47,7 +57,6 @@ public class StandbyActivity extends BaseActivity {
         mLocationClient.registerLocationListener(new StandbyLocationListener());
         setContentView(R.layout.activity_standby);
         init();
-        queryWeather(cityname);
         layout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -93,7 +102,7 @@ public class StandbyActivity extends BaseActivity {
         public void onReceiveLocation(BDLocation bdLocation) {
             Log.d(TAG, "onReceiveLocation: "+ bdLocation.getCity());
             location_textView.setText(bdLocation.getCity());
-            queryWeather(cityname);
+            requestWeather(bdLocation.getCity());
         }
     }
 
@@ -103,35 +112,47 @@ public class StandbyActivity extends BaseActivity {
         mLocationClient.stop();
     }
 
-    public void queryWeather(String cityName){
-        RequestQueue queue= Volley.newRequestQueue(this);
-        JsonObjectRequest request=new JsonObjectRequest(
-                "https://free-api.heweather.com/s6/weather?location="+cityName+"&key=537664b7e2124b3c845bc0b51278d4af",
-                null, new Response.Listener<JSONObject>() {
+    public void requestWeather(String cityName) {
+        String weatherUrl = "https://free-api.heweather.com/s6/weather?location="+cityName+"&key=537664b7e2124b3c845bc0b51278d4af";
+        HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
             @Override
-            public void onResponse(JSONObject jsonObject) {
-
-                JSONArray jsonArray= null;
-                Log.d(TAG, jsonObject.toString());
-                try {
-                    jsonArray = jsonObject.getJSONArray("HeWeather6");
-                    String weatherContent=jsonArray.getJSONObject(0).toString();
-                    Weather weather=new Gson().fromJson(weatherContent,Weather.class);
-                    weatherIcon.setImageResource(HandlerWeatherUtil.getWeatherImageResource(Integer.parseInt(weather.now.code)));
-                    standby_weather_tmp.setText(weather.forecastList.get(0).max+" / "+weather.forecastList.get(0).min+"℃");
-                    standby_life_clothes.setText("穿衣：" + weather.lifestyleList.get(1).brf);
-                    standby_life_car.setText("洗车：" + weather.lifestyleList.get(6).brf);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+            public void onResponse(Call call, okhttp3.Response response) throws IOException {
+                final String responseText = response.body().string();
+                final Weather weather = Utility.handleWeatherResponse(responseText);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (weather != null) {
+                            showWeatherInfo(weather);
+                        } else {
+                            Toast.makeText(StandbyActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
-        }, new Response.ErrorListener() {
+
             @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                Log.e(TAG,volleyError.getMessage(),volleyError );
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(StandbyActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
-        queue.add(request);
-        Log.d(TAG, "queryWeather: ");
+    }
+
+    private void showWeatherInfo(Weather weather) {
+        if(weather.now.code != null) {
+            Log.i(TAG, "onResponse: weather.now.code = " + weather.now.code);
+            weatherIcon.setImageResource(HandlerWeatherUtil.getWeatherImageResource(Integer.parseInt(weather.now.code)));
+        }else {
+            weatherIcon.setImageResource(R.drawable.weather_unknown);
+        }
+        standby_weather_tmp.setText(weather.forecastList.get(0).max + " / " + weather.forecastList.get(0).min + "℃");
+        standby_life_clothes.setText("穿衣：" + weather.lifestyleList.get(1).brf);
+        standby_life_car.setText("洗车：" + weather.lifestyleList.get(6).brf);
     }
 }
